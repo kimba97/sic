922aeb2814f11d64c31606f5d2a3f15731582667
kimba <jogapp0@gmail.com>
2018-11-01 23:25:22 -0600

    CRUD hecho me la pelan ojetes jajajajaj xdddddd

--- apps/sistemacontable/admin.py
+++ apps/sistemacontable/admin.py
@@ -3,4 +3,23 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import Cuenta, Partida, Periodo, Catalogo
+
 # Register your models here.
+class CuentaAdmin(admin.ModelAdmin):
+    list_display = ('nombre','codigo','naturaleza','debe','haber')
+admin.site.register(Cuenta,CuentaAdmin)
+
+class PartidaAdmin(admin.ModelAdmin):
+    list_display = ('fecha','cuenta','descripcion','monto')
+admin.site.register(Partida,PartidaAdmin)
+
+class PeriodoAdmin(admin.ModelAdmin):
+    list_display = ('get_partidas',)
+admin.site.register(Periodo,PeriodoAdmin)
+
+class CatalogoAdmin(admin.ModelAdmin):
+    list_display = ('get_cuentas',)
+admin.site.register(Catalogo,CatalogoAdmin)
+
+admin.site.site_header = 'Sistema Contable'
--- apps/sistemacontable/models.py
+++ apps/sistemacontable/models.py
@@ -4,3 +4,47 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Cuenta(models.Model):
+    nombre = models.CharField(max_length=30)
+    codigo = models.IntegerField()
+    naturaleza = models.CharField(max_length=30)
+    debe = models.FloatField()
+    haber = models.FloatField()
+
+    class Meta:
+        verbose_name='Cuenta'
+        verbose_name_plural='Cuentas'
+    def __str__(self):
+        return '%s' %(self.nombre)
+
+class Partida(models.Model):
+    fecha = models.DateField()
+    cuenta = models.ForeignKey(Cuenta)
+    descripcion = models.TextField(max_length=500)
+    monto = models.FloatField()
+
+    class Meta:
+        verbose_name='Partida'
+        verbose_name_plural='Partidas'
+    def __str__(self):
+        return '%s' %(self.id)
+
+class Periodo(models.Model):
+    partidas = models.ManyToManyField(Partida)
+    def get_partidas(self):
+        return "\n".join([i.id for i in self.partidas.all()])
+    class Meta:
+        verbose_name='Periodo'
+        verbose_name_plural='Periodos'
+    def __str__(self):
+        return '%s' %(self.id)
+
+class Catalogo(models.Model):
+    cuentas = models.ManyToManyField(Cuenta)
+    def get_cuentas(self):
+        return "\n".join([i.codigo for i in self.cuentas.all()])
+    class Meta:
+        verbose_name='Catalogo'
+        verbose_name_plural='Catalogos'
+    def __str__(self):
+        return '%s' %(self.id)
